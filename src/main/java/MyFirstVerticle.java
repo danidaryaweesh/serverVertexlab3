1329277e72dd6b0e04a9ef8b582ce637d1b6bfa5
danidaryaweesh <dani_kurkees@hotmail.com>
2016-12-08 12:42:48 +0100

    database working!

--- src/main/java/MyFirstVerticle.java
+++ src/main/java/MyFirstVerticle.java
@@ -1,5 +1,9 @@
+import com.mongodb.*;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+
+import java.net.UnknownHostException;
+import java.util.Date;
 
 /**
  * Created by dani on 2016-12-08.
@@ -11,6 +15,28 @@ public class MyFirstVerticle extends AbstractVerticle {
         vertx
                 .createHttpServer()
                 .requestHandler(r -> {
+                    try {
+                        System.out.println("Handling mongo!");
+                        MongoClient mongo = new MongoClient( "localhost" , 27017 );
+                        DB db = mongo.getDB("firstDatabase");
+                        DBCollection table = db.getCollection("user");
+                        BasicDBObject document = new BasicDBObject();
+                        document.put("name", "Alican");
+                        document.put("age", 21);
+                        document.put("createdDate", new Date());
+                        table.insert(document);
+
+                        BasicDBObject searchQuery = new BasicDBObject();
+                        searchQuery.put("name", "Alican");
+
+                        DBCursor cursor = table.find(searchQuery);
+
+                        while (cursor.hasNext()) {
+                            System.out.println("got: "+cursor.next());
+                        }
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    }
                     r.response().end("<h1>Hello from my first " +
                             "Vert.x 3 application with first changes now!</h1>");
                 })
