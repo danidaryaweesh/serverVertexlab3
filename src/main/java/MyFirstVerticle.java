77a5b412a87ac3ede9d7d247f07b770bac0d6d44
danidaryaweesh <dani_kurkees@hotmail.com>
2016-12-19 14:48:13 +0100

    vertx server fully working with latest vertx version!

--- src/main/java/MyFirstVerticle.java
+++ src/main/java/MyFirstVerticle.java
@@ -1,19 +1,19 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.ServerWebSocket;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
+
+import java.util.ArrayList;
+
 /**
  * Created by dani on 2016-12-08.
  */
 
-import org.vertx.java.core.Handler;
-import org.vertx.java.core.eventbus.EventBus;
-import org.vertx.java.core.eventbus.Message;
-import org.vertx.java.core.http.HttpServer;
-import org.vertx.java.core.http.ServerWebSocket;
-import org.vertx.java.core.json.JsonArray;
-import org.vertx.java.core.json.JsonObject;
-import org.vertx.java.platform.Verticle;
-
-import java.util.ArrayList;
-
-public class MyFirstVerticle extends Verticle {
+public class MyFirstVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
@@ -21,6 +21,11 @@ public class MyFirstVerticle extends Verticle {
         HttpServer serverSocket = vertx.createHttpServer();
         final EventBus usersBus = vertx.eventBus();
         final ArrayList<User> myInstances = new ArrayList<>();
+        final ArrayList<String> loggedIDS = new ArrayList<>();
+
+        final JsonObject mongoconfig = new JsonObject().put("connection_string", "mongodb://localhost:27017").put("db_name", "vertxusers");
+        final MongoClient mongoClient = MongoClient.createShared(vertx, mongoconfig);
+
         // websocket on
         serverSocket.websocketHandler(new Handler<ServerWebSocket>() {
             // websocket connected
@@ -32,143 +37,156 @@ public class MyFirstVerticle extends Verticle {
 
                 final StringBuffer username = new StringBuffer();
                 final StringBuffer id = new StringBuffer();
-
-                final Handler<Message> messageHandler = new Handler<Message>() {
-                    @Override
-                    public void handle(Message message) {
-                        serverWebSocket.writeTextFrame(message.body().toString());
-                    }
-                };
+                final StringBuffer loggedin = new StringBuffer();
 
                 // if client want to end / close socket
                 serverWebSocket.closeHandler(e -> {
                     System.out.println("Closed socket... in close handler now");
 
                     // remove right user
-                    for(int i=0;i<myInstances.size();i++){
-                        System.out.println("index i:" +myInstances.get(i));
-                        if(myInstances.get(i).getId().equals(id.toString())){
+                    for (int i = 0; i < myInstances.size(); i++) {
+                        System.out.println("index i:" + myInstances.get(i));
+                        if (myInstances.get(i).getId().equals(id.toString())) {
                             myInstances.remove(i);
                             System.out.println("After remove");
-                            for(int a=0;a<myInstances.size();a++){
-                                System.out.println("instance "+myInstances.get(a).getId());
+                            for (int a = 0; a < myInstances.size(); a++) {
+                                System.out.println("instance " + myInstances.get(a).getId());
                             }
                             break;
                         }
                     }
 
-                    System.out.println("after removing the username: "+username.toString());
-                    usersBus.publish("chat", new JsonObject().putString("offline", username.toString()));
-                    usersBus.unregisterHandler("chat", messageHandler);
-                    usersBus.unregisterHandler("chat/" + id.toString(), messageHandler);
+                    for(int i=0;i<loggedIDS.size();i++){
+                        if(loggedIDS.get(i).equals(id.toString())){
+                            loggedIDS.remove(i);
+                        }
+                    }
 
+                    System.out.println("after removing the username: " + username.toString());
+                    usersBus.publish("chat", new JsonObject().put("offline", username.toString()));
+
+
+                    usersBus.consumer("chat/" + id.toString()).unregister(res -> {
+                        if (res.succeeded()) {
+                            System.out.println("The handler un-registration has reached all nodes");
+                        } else {
+                            System.out.println("Un-registration failed!");
+                        }
+                    });
                 });
 
                 // handle the data (json object that the user sends)
-                serverWebSocket.dataHandler(buffer -> {
+                serverWebSocket.handler(buffer -> {
                     System.out.println("before jsonobject but in datahandler");
                     JsonObject object = new JsonObject(buffer.toString());
-                    System.out.println("After jsonobject datahandler and object is: "+object.toString());
-                    System.out.println("Username now is: "+username.toString());
 
-                    if (username.toString().equals("") ) { // om användaren inte är registrerad
+                    if(object.getString("option").equals("register")) {
+                        JsonObject document = new JsonObject().put("username", object.getValue("name")).put("password", object.getValue("password"));
+                        mongoClient.insert("vertxusers", document, insertResult -> {
+                            if (insertResult.succeeded()) {
+                                System.out.println("Succeded to insert the user with id: " + insertResult.result());
+                                loggedIDS.add(object.getString("id"));
+                            } else {
+                                System.out.println("Failed to register user and will return");
+                            }
+                        });
+                    }
 
+                    else if(object.getString("option").equals("login")){
+                        JsonObject document = new JsonObject().put("username", object.getValue("name")).put("password", object.getValue("password"));
+                        mongoClient.find("vertxusers", document, res -> {
+                            if (res.succeeded()) {
+                                System.out.println("Succeded to run!");
+                                if(res.result().size() > 0){
+                                    System.out.println("Found user and continue and will add id..");
+                                    loggedIDS.add(object.getString("id"));
 
-                        // database.....
-                        username.append(object.getString("name")); // kan append null istället
-                        id.append(object.getString("id"));
-                        String preferedUser = object.getString("to");
-                        System.out.println("Prefered user: "+preferedUser +" and id is from stringbuffer: "+id.toString() + " id in the json object: "+object.getString("id"));
-                        User user = new User(username.toString(),  object.getString("id"),object.getString("preferedUser")!=null? object.getString("preferedUser") : "", false);
-                        user.setPreferedUser(preferedUser);
-                        myInstances.add(user);
-
-                        usersBus.publish("chat", new JsonObject().putString("user", object.getString("name")).toString());
-
-                        usersBus.registerHandler("chat/"+user.getId(), messageHandler);
-                        usersBus.registerHandler("chat", messageHandler);
-
-                        for(int i=0;i<myInstances.size();i++){
-                            if(!myInstances.get(i).isBusy() && myInstances.get(i).getTo().equals("") && myInstances.get(i).getPreferedUser().equals(username.toString())){
-                                // send my id to the other user
-                                System.out.println("IN IF BITCH!");
-                                myInstances.get(i).setTo(username.toString());
-                                JsonObject message = new JsonObject().putString("toID", id.toString());
-                                myInstances.get(i).setBusy(true);
-                                System.out.println("THE WHOLE MYINSTANCE THAT I GOT: id:"+myInstances.get(i).getId() + " ,username: "+myInstances.get(i).getUsername() + " ,prefereduser: "+myInstances.get(i).getPreferedUser() + " ,to: "+myInstances.get(i).getTo());
-                                System.out.println("Send my id to other user:"+id.toString());
-
-                                // update myself!
-                                for(int num=0;num<myInstances.size();num++){
-                                    if(id.toString().equals(myInstances.get(num).getId())){
-                                        myInstances.get(num).setBusy(true);
-                                        myInstances.get(num).setTo(myInstances.get(i).getUsername());
-                                        break;
-                                    }
+                                }else{
+                                    System.out.println("Didnt find the user and will return!");
+                                    serverWebSocket.writeFinalTextFrame("/wrong password");
                                 }
-                                break;
+                            } else {
+                                res.cause().printStackTrace();
+                            }
+                        });
+                    }
+
+                    else if (username.toString().equals("") && object.getString("option").equals("connect")) { // om användaren inte är registrerad
+
+                        System.out.println("***********************"+ "   in connect   "+"*******************");
+                        for (int i = 0; i < loggedIDS.size(); i++) {
+                            if (loggedIDS.get(i).equals(object.getString("id"))) {
+                                System.out.println("FOUND THE ID!");
+                                loggedin.append("true");
                             }
                         }
 
-                        System.out.println("Registerd client name as: "+object.getString("name") + "  and with id: "+object.getString("id"));
-
-                        System.out.println("if got: "+messageHandler.toString());
-                    }else{ // om användaren är registrerad --> skicka meddelandet direkt
-                        JsonObject message = new JsonObject()
-                                .putString("from", object.getString("from"))
-                                .putString("to", object.getString("to"))
-                                .putString("body", object.getString("body"));
-                        System.out.println("Else got: "+message.toString());
-                        System.out.println("Else got to: "+object.getString("to")  + "  and the current username who is trying to send is : "+username.toString());
-                        for(int i=0;i<myInstances.size();i++){
-                            if(myInstances.get(i).isBusy() && myInstances.get(i).getTo().equals(object.getString("from")) && myInstances.get(i).getPreferedUser().equals(username.toString()) && object.getString("to").equals(myInstances.get(i).getUsername()) && !object.getString("id").equals(myInstances.get(i).getId())){
-                                System.out.println("Alican found with id: "+myInstances.get(i).getId()+ " busy?: "+myInstances.get(i).isBusy());
-                                usersBus.publish("chat/" + myInstances.get(i).getId(), message.toString());
+                        if(loggedin.toString().equals("")){
+                            for(int nr=0;nr<myInstances.size();nr++){
+                                if(object.getString("name").equals(myInstances.get(nr).getUsername())){
+                                    loggedin.append("true");
+                                }
                             }
-                            System.out.println("THE WHOLE MYINSTANCE THAT I GOT: id:"+myInstances.get(i).getId() + " ,username: "+myInstances.get(i).getUsername() + " ,prefereduser: "+myInstances.get(i).getPreferedUser() + " ,to: "+myInstances.get(i).getTo()+" busy?: "+myInstances.get(i).isBusy());
+                        }
+
+                        if(loggedin.toString().equals("true")){
+                            System.out.println("***********************"+ "   in if   "+"*******************");
+
+                            username.append(object.getString("name"));
+                            id.append(object.getString("id"));
+                            String preferedUser = object.getString("to");
+
+                            User user = new User(username.toString(), object.getString("id"), object.getString("preferedUser") != null ? object.getString("preferedUser") : "", false);
+                            user.setPreferedUser(preferedUser);
+                            myInstances.add(user);
+                            usersBus.publish("chat", new JsonObject().put("user", object.getString("name")).toString());
+
+                            usersBus.consumer("chat/" + user.getId(), message -> {
+                                System.out.println("The conusmer handler have received a message: " + message.body() + "The user id is: " + user.getId());
+                                serverWebSocket.writeFinalTextFrame(message.body().toString());
+                            }).completionHandler(res -> {
+                                if (res.succeeded()) {
+                                    System.out.println("The handler registration has reached all nodes 1");
+                                } else {
+                                    System.out.println("Registration failed!");
+                                }
+                            });
+
+                            for (int i = 0; i < myInstances.size(); i++) {
+                                if (!myInstances.get(i).isBusy() && myInstances.get(i).getTo().equals("") && myInstances.get(i).getPreferedUser().equals(username.toString())) {
+                                    // send my id to the other user
+                                    System.out.println("FOUND THE USER ON REGISTRATION!");
+                                    myInstances.get(i).setTo(username.toString());
+                                    myInstances.get(i).setBusy(true);
+                                    System.out.println("THE WHOLE MYINSTANCE THAT I GOT while connecting: id:" + myInstances.get(i).getId() + " ,username: " + myInstances.get(i).getUsername() + " ,prefereduser: " + myInstances.get(i).getPreferedUser() + " ,to: " + myInstances.get(i).getTo());
+
+                                    // update myself!
+                                    for (int num = 0; num < myInstances.size(); num++) {
+                                        if (myInstances.get(num).getId().equals(id.toString())) {
+                                            System.out.println("Found myself and i will update myself!");
+                                            myInstances.get(num).setBusy(true);
+                                            myInstances.get(num).setTo(myInstances.get(i).getUsername());
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+
+                    }else if(loggedin.toString().equals("true")){ // om användaren är registrerad --> skicka meddelandet direkt
+                        System.out.println("Else got: " + object.getString("from")+":"+object.getString("body"));
+                        for (int i = 0; i < myInstances.size(); i++) {
+                            if (myInstances.get(i).isBusy() && myInstances.get(i).getTo().equals(object.getString("from")) && myInstances.get(i).getPreferedUser().equals(username.toString()) && object.getString("to").equals(myInstances.get(i).getUsername()) && !object.getString("id").equals(myInstances.get(i).getId())) {
+                                System.out.println("found with id: " + myInstances.get(i).getId() + " busy?: " + myInstances.get(i).isBusy());
+                                usersBus.publish("chat/" + myInstances.get(i).getId(), object.getString("from")+":"+object.getString("body"));
+                            }
+                            System.out.println("THE WHOLE MYINSTANCE THAT I GOT: id:" + myInstances.get(i).getId() + " ,username: " + myInstances.get(i).getUsername() + " ,prefereduser: " + myInstances.get(i).getPreferedUser() + " ,to: " + myInstances.get(i).getTo() + " busy?: " + myInstances.get(i).isBusy());
                         }
                     }
                 });
             }
-        }).listen(1337, "localhost");
+        }).listen(1337, "130.229.183.91");
     }
 }
 
-/*   try {
-                        System.out.println("Handling mongo!");
-                        MongoClient mongo = new MongoClient( "localhost" , 27017 );
-                        DB db = mongo.getDB("firstDatabase");
-                        DBCollection table = db.getCollection("user");
-                        BasicDBObject document = new BasicDBObject();
-                        document.put("name", "Alican");
-                        document.put("age", 21);
-                        document.put("createdDate", new Date());
-                        table.insert(document);
-
-                        BasicDBObject searchQuery = new BasicDBObject();
-                        searchQuery.put("name", "Alican");
-
-                        DBCursor cursor = table.find(searchQuery);
-
-                        while (cursor.hasNext()) {
-                            System.out.println("got: "+cursor.next());
-                        }
-                    } catch (UnknownHostException e) {
-                        e.printStackTrace();
-                    }
-                    r.response().end("<h1>Hello from my first " +
-                            "Vert.x 3 application with first changes now!</h1>");
- */
-
-/*        MongoClient mongo;
-        DB db;
-        DBCollection table;
-
-        try {
-            mongo = new MongoClient( "localhost" , 27017 );
-            db= mongo.getDB("firstDatabase");
-            table= db.getCollection("user");
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-*/
--- src/test/java/MyFirstVerticleTest.java
+++ src/test/java/MyFirstVerticleTest.java
@@ -1,47 +1,89 @@
-import io.vertx.core.Vertx;
-import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 /**
  * Created by dani on 2016-12-08.
  */
-@RunWith(VertxUnitRunner.class)
+
 public class MyFirstVerticleTest {
 
-    private Vertx vertx;
 
-    @Before
-    public void setUp(TestContext context) {
-      /*  vertx = Vertx.vertx();
-        vertx.deployVerticle(MyFirstVerticle.class.getName(),
-                context.asyncAssertSuccess());
 
-    */
-        System.out.printf("bob");
-    }
+ /*
 
-    @After
-    public void tearDown(TestContext context) {
-        //vertx.close(context.asyncAssertSuccess());
-        System.out.printf("bob after");
-    }
 
-    @Test
-    public void testMyApplication(TestContext context) {
-       /* final Async async = context.async();
 
-        vertx.createHttpClient().getNow(8090, "localhost", "/",
-                response -> {
-                    response.handler(body -> {
-                        context.assertTrue(body.toString().contains("Hello"));
-                        async.complete();
-                    });
-                });*/
-        System.out.printf("bob test");
-    }
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.http.ServerWebSocket;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.platform.Verticle;
+
+import java.util.ArrayList;
+
+
+<dependency>
+            <groupId>io.vertx</groupId>
+            <artifactId>vertx-core</artifactId>
+            <version>2.1.2</version>
+            <scope>provided</scope>
+        </dependency>
+
+        <dependency>
+            <groupId>io.vertx</groupId>
+            <artifactId>vertx-platform</artifactId>
+            <version>2.1.2</version>
+            <scope>provided</scope>
+        </dependency>
+
+        <dependency>
+            <groupId>io.vertx</groupId>
+            <artifactId>vertx-platform</artifactId>
+            <version>3.0.0</version>
+            <scope>provided</scope>
+        </dependency>
+
+
+
+
+   try {
+                        System.out.println("Handling mongo!");
+                        MongoClient mongo = new MongoClient( "localhost" , 27017 );
+                        DB db = mongo.getDB("firstDatabase");
+                        DBCollection table = db.getCollection("user");
+                        BasicDBObject document = new BasicDBObject();
+                        document.put("name", "Alican");
+                        document.put("age", 21);
+                        document.put("createdDate", new Date());
+                        table.insert(document);
+
+                        BasicDBObject searchQuery = new BasicDBObject();
+                        searchQuery.put("name", "Alican");
+
+                        DBCursor cursor = table.find(searchQuery);
+
+                        while (cursor.hasNext()) {
+                            System.out.println("got: "+cursor.next());
+                        }
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    }
+                    r.response().end("<h1>Hello from my first " +
+                            "Vert.x 3 application with first changes now!</h1>");
+
+
+       MongoClient mongo;
+        DB db;
+        DBCollection table;
+
+        try {
+            mongo = new MongoClient( "localhost" , 27017 );
+            db= mongo.getDB("firstDatabase");
+            table= db.getCollection("user");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+*/
+
 
 }
